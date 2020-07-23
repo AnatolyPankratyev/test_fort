3f015637a057a625cdc7fbc1bc72e6312ca5a5fa
IanFoxStudio <ianfoxstudio@gmail.com>
2020-07-23 18:01:55 +0300

    start project

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'No version set (parsed as 1.0.0)',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'No version set (parsed as 1.0.0)',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'gabordemooij/redbean' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'c48fdaefceea25ed42e6915dafec6c8023706009',
+    ),
+  ),
+);
